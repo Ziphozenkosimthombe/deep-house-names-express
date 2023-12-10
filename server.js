7f2af02ac1c2036af4bd4d7e7365d2b668b38aa3
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-10 22:53:30 +0200

    changes

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
+/*
+=========REQUIRED DEPENDECIES===========
+*/
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 require('dotenv').config()
 
-
+//declare the db variable
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'deep-house-names'
 
+//connecting to the MONGO database
 MongoClient.connect(dbConnectionStr)
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
+/*
+========MADDLEWARE==========
+*/    
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
