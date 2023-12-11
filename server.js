e694a5e4edf9bcbe0c4b22578c1eefaa91be2a20
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-11 22:57:05 +0200

    solutions...

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 //declare the db variable
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'deep-house-names'
+    dbName = 'star-wars-quotes'
 
 //connecting to the MONGO database
 MongoClient.connect(dbConnectionStr)
@@ -26,6 +26,32 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+/*
+==============ROUTES==========
+*/ 
+
+app.get('/', (req, res)=>{
+    db.collection('taxi-rank-info').find().toArray()
+        .then(data =>{
+            let nameList = data.map(item => item.specialName)
+            console.log(nameList)
+            res.render('index.ejs', {info: nameList})
+        })
+        .catch(error => console.error(error))
+})
+
+app.post('/api', (req, res) => {
+    console.log('post success')
+    db.collection('taxi-rank-info').insertOne(
+        req.body
+    )
+        .then(result=>{
+            console.log(result)
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
+})
 
 
 app.listen(process.env.PORT || PORT, ()=>{
